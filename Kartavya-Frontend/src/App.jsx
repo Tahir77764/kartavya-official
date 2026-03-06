7f89ddbe63fbb96166cd891bf7ac846efa2afada
Tahir77764 <tahirbaba907199@gmail.com>
2026-03-06 13:23:46 +0530

    First Initial commit

---
+++ Kartavya-Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Assets/Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+export default App;
---
+++ Kartavya-Frontend/src/Assets/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold text-white">Kartavya</h1>
+        </>
+    );
+}
+export default Navbar;
